433e80acac2ec1621eadb46f4071b1d9ddda80e9
RexSocrates <socrateshung053@gmail.com>
2017-05-23 13:07:46 +0800

    Add Doctor model

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// import the model
+use App\Doctor;
+
+class AccountController extends Controller
+{
+    //
+    public function addNewDoctor() {
+        
+    }
+}
