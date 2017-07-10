fef59083fd31368982f89277b87c855ea2f14975
Harry <Harry@Harryde-MacBook-Pro.local>
2017-07-10 14:48:47 +0800

    Complete model

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/schedule', 'ScheduleController@schedule');
+
 Route::get('/reservation', 'AccountController@reservation');
 
 Route::get('/reservation/updateReservation/{id}', function() {
