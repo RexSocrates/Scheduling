61105011b97a37e499ef1516bcdedb1d49944be1
RexSocrates <socrateshung053@gmail.com>
2017-07-17 15:41:37 +0800

    Update schedule model

--- app/Schedule.php
+++ app/Schedule.php
@@ -9,114 +9,75 @@ class Schedule extends Model
 {
 	protected $table = 'Schedule';
 
-    //查看 班表資訊
-	// public function scheduleList()
-	// {
-	// 	$schedule = DB::table('Schedule')->get();
-
-	// 	return $schedule;
-	// }
-
-    //查看 單一班表資訊
-    public function scheduleData($scheduleID)
-    {
+    //取得所有班表資訊
+	public function getSchedule() {
         $schedule = DB::table('Schedule')
-        ->where("scheduleID",$scheduleID)
-        ->first();
-
+            ->get();
+        
         return $schedule;
     }
 
+    //透過班ID取得單一班表資訊
+    public function getScheduleDataByID($scheduleID) {
+        $schedule = DB::table('Schedule')
+            ->where('scheduleID', $scheduleID)
+            ->first();
+        
+        return $schedule;
+    }
+    
+    
+    // 新增一比上班資料
+    public function addSchedule(array $date) {
+        $newScheduleID = DB::table('Schedule')->insertGetId([
+            'doctorID' => $data['doctorID'],
+            'shiftName' => $data['shiftName'],
+            'categorySerial' => $data['categorySerial'],
+            'isWeekday' => $data['isWeekday'],
+            'location' => $data['location'],
+            'date' => $data['date'],
+            'endDate' => $data['endDate'],
+            'confirmed' => $data['confirmed'],
+        ]);
+        
+        return $newScheduleID;
+    }
+    
+    // 透過班ID更新單一個班
+    public function updateScheduleByID($scheduleID, array $data) {
+        $affectedRows = DB::table('Schedule')
+            ->where('scheduleID', $scheduleID)
+            ->update([
+                'doctorID' => $data['doctorID'],
+                'shiftName' => $data['shiftName'],
+                'categorySerial' => $data['categorySerial'],
+                'isWeekday' => $data['isWeekday'],
+                'location' => $data['location'],
+                'date' => $data['date'],
+                'endDate' => $data['endDate'],
+                'confirmed' => $data['confirmed'],
+            ]);
+        
+        return $affectedRows;
+    }
+    
+    // 確認當月班表
+    public function confirmSchedule() {
+        $currentMonthStr = date('Y-m')
+        
+        $affectedRows = DB::table('Schedule')
+            ->where('date', 'like', $currentMonthStr.'%')
+            ->update([
+                'confirmed' => true
+            ]);
+        
+        return $affectedRows;
+    }
 
-
-    //查看 新增班表
-	// public function addSchedule($isWeekday, $location, $category, $date, $confirmed){
- //        $generatedSerial = 0;
-
- //        $count = DB::table("Schedule")
- //            -> where('doctorID',$doctorID) 
- //            -> where('periodSerial',$periodSerial) 
- //            -> where('isWeekday',$isWeekday)
- //            -> where('location',$location) 
- //            -> where('category',$category)
- //            -> where('date',$date)
- //            -> where('confirmed',$confirmed)
- //            ->count();
- //        if($count==0){
-        //新增預班
-        //         $generatedSerial = DB::table('Schedule')-> insertGetId([
-        //         'doctorID' => $doctorID,
-        //         'periodSerial' => $periodSerial,
-        //         'isWeekday' => $isWeekday,
-        //         'location' => $location,
-        //         'category' => $category,
-        //         'date' => $date, 
-        //         'confirmed' => $confirmed      
-        //     ]);
-        // }
-        // else{
-            //得到預班表id
-    //             $generatedSerial = DB::table("Schedule")
-    //             -> where('doctorID',$doctorID)
-    //             -> where('periodSerial',$periodSerial) 
-    //             -> where('isWeekday',$isWeekday)
-    //             -> where('location',$location) 
-    //             -> where('category',$category)
-    //             -> where('date',$date)
-    //             -> where('confirmed',$confirmed)
-    //             -> value('scheduleID');
-    //     }
-
-    // 	return $generatedSerial;
-    // }
-
-    // public function updateSchedule($scheduleID, $doctorID,$isWeekday, $location, $category, $date, $confirmed){
-
-    //     $generatedSerial ＝ 0;
-
-    //     $count = DB::table("Schedule")
-    //             -> where('doctorID',$doctorID)
-    //             -> where('periodSerial',$periodSerial) 
-    //             -> where('isWeekday',$isWeekday)
-    //             -> where('location',$location) 
-    //             -> where('category',$category)
-    //             -> where('date',$date)
-    //             -> where('confirmed',$confirmed)
-    //             ->count();
-    //         if($count==0){
-    //             //新增班表
-    //             $generatedSerial = DB::table('Schedule')-> insertGetId([
-    //             'doctorID' => $doctorID,
-    //             'periodSerial' => $periodSerial,
-    //             'isWeekday' => $isWeekday,
-    //             'location' => $location,
-    //             'category' => $category,
-    //             'date' => $date, 
-    //             'confirmed' => $confirmed  
-                    
-    //         ]);
-
-    //         }
-               
-    //         else{
-                //取得班表id
-    //             $generatedSerial = DB::table("Schedule")
-    //             -> where('doctorID',$doctorID)
-    //             -> where('periodSerial',$periodSerial) 
-    //             -> where('isWeekday',$isWeekday)
-    //             -> where('location',$location) 
-    //             -> where('category',$category)
-    //             -> where('date',$date)
-    //             -> where('confirmed',$confirmed)
-    //             -> value('scheduleID');
-    //         }
-    //     return $generatedSerial;
-    // }
-
-    // public function deleteSchedule($scheduleID){
-    //     $generatedSerial = DB::table("Schedule")
-    //     $generatedSerial = DB::where('scheduleID','==',  scheduleID) -> delete()
-
-    //     return $generatedSerial;
-    // }
+    // 透過班ID刪除單一紀錄
+    public function deleteScheduleByID($scheduleID) {
+        DB::table('Schedule')
+            ->where('scheduleID', $scheduleID)
+            ->delete();
+    }
 }
