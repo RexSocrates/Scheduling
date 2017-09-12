e57b1f9459c16ac75438a664749288b531efd915
benjaminchan66 <chanben1944@yahoo.com.hk>
2017-09-12 22:28:15 +0800

    隱藏設定 主頁公告加入 文字過長溢出變"..."

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -28,7 +28,8 @@
     						    	</div>
     						    	<div class="col s10">
     						    		<span class="card-title">{{ $announcement->title }}<a class="dropdown-edit-button right" href="" data-activates='dropdown-announcement'><i class="material-icons" onclick="passAnnouncementSerial({{ $announcement->announcementSerial }})">more_vert</i></a></span>
-    						    		<p>{{ $announcement->content }}<a href="#modal-more" onclick="getAnnouncement({{ $announcement->announcementSerial }})">more</a></p>
+    						    		<p class="announcement-ellipsis">{{ $announcement->content }}</p>
+    						    		<a href="#modal-more" onclick="getAnnouncement({{ $announcement->announcementSerial }})">more</a>
     						    	</div>
                                     
     				            </div>
