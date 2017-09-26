a1a766984e0626ce32f4dea9597a5e8f46707f1e
benjaminchan66 <chanben1944@yahoo.com.hk>
2017-09-26 21:47:04 +0800

    前端小更改

--- resources/views/pages/first-edition-situation.blade.php
+++ resources/views/pages/first-edition-situation.blade.php
@@ -15,7 +15,7 @@
                 <div class="col s12 m12">
       		  	  	<div class="card">
       		  	  		<div class="card-action">
-      		  	  			<font class="card-title">初版班表XX</font>
+      		  	  			<font class="card-title">初版班表排班現況</font>
       		  	  		</div>
       		  	  		<div class="divider"></div>
       		  	  	  	<div class="card-content">
@@ -37,9 +37,8 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                 @foreach($mustOnDuty as $duty)
+                                    @foreach($mustOnDuty as $duty)
                                     <tr>
-                                   
                                         <th>{{ $duty['doctorName'] }}</th>
                                         <th>應上</th>
                                         <th>{{ $duty['totalShift'] }}</th>
@@ -52,11 +51,10 @@
                                         <th>{{ $duty['weekendShifts'] }}</th>
                                    
                                     </tr>
-                                     @endforeach
+                                    @endforeach
                                     
-                                     @foreach($onDuty as $duty)
+                                    @foreach($onDuty as $duty)
                                     <tr>
-                                   
                                         <th>{{ $duty['doctorName'] }}</th>
                                         <th>已上</th>
                                         <th>{{ $duty['totalShift'] }}</th>
@@ -67,12 +65,8 @@
                                         <th>{{ $duty['day'] }}</th>
                                         <th>{{ $duty['night'] }}</th>
                                         <th>{{ $duty['weekendShifts'] }}</th>
-                                   
                                     </tr>
-                                     @endforeach
-                                   
-                                    
-                                    
+                                    @endforeach
                                 </tbody>
                             </table>
       		  	  	  	</div>
@@ -81,7 +75,6 @@
       		</div>
 		</div>
 	</div>
-	
 @endsection
 
 @section('script')
